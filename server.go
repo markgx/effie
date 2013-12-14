017410aa0cbd73ee81ed344e2a0bd4910e7e9c3b
Mark Guerra <markgx@gmail.com>
2013-12-13 23:52:18 -0800

    session support

--- server.go
+++ server.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/render"
-	"net/http"
+	"github.com/codegangsta/martini-contrib/sessions"
 )
 
 func main() {
 	m := martini.Classic()
+
+	store := sessions.NewCookieStore([]byte("changethis"))
+	m.Use(sessions.Sessions("effie_session", store))
 
 	m.Use(render.Renderer(render.Options{
 		Directory: "templates",
