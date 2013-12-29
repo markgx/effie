ce717901ce43a98b402481a722b41db375d3c33f
Mark Guerra <markgx@gmail.com>
2013-12-28 18:21:48 -0800

    password hashing

--- handlers/authentication.go
+++ handlers/authentication.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"crypto/sha256"
 	"effie/repositories"
+	"encoding/base64"
 	"fmt"
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/codegangsta/martini-contrib/sessions"
@@ -21,15 +23,14 @@ func Login(r render.Render) {
 func LoginPost(w http.ResponseWriter, req *http.Request, loginForm LoginForm, rdbSession *r.Session, session sessions.Session) string {
 	userRepository := repositories.UserRepository{Session: rdbSession}
 
-	// TODO: verify log in
-
 	user, err := userRepository.FindByUsername(loginForm.Username)
 
 	if err != nil {
+		// TODO: show error
 		panic(err)
 	}
 
-	if user != nil {
+	if user != nil && hashString(loginForm.Password) == user.PasswordHash {
 		session.Set("user_id", user.ID)
 
 		if returnUrl := session.Get("return_url"); returnUrl != nil {
@@ -40,10 +41,17 @@ func LoginPost(w http.ResponseWriter, req *http.Request, loginForm LoginForm, rd
 		http.Redirect(w, req, "/admin", 301)
 	}
 
+	// TODO: authentication failed, show error
 	return fmt.Sprintf("U:%s P:%s v:%+v", loginForm.Username, loginForm.Password, user)
 }
 
 func LogOut(w http.ResponseWriter, req *http.Request, session sessions.Session) {
 	session.Delete("user_id")
 	http.Redirect(w, req, "/login", 301)
+}
+
+func hashString(s string) string {
+	hb := sha256.Sum256([]byte(s))
+	h := base64.URLEncoding.EncodeToString(hb[:])
+	return h
 }
--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID       string `gorethink:"id,omitempty"`
-	Username string `gorethink:"username"`
-	Password string `gorethink:"password"`
+	ID           string `gorethink:"id,omitempty"`
+	Username     string `gorethink:"username"`
+	PasswordHash string `gorethink:"passwordHash"`
 }
